42f6a8bee9f56849f20d95ef413d9ec5f842186a
matthewjohnson <mjohns91@gmail.com>
2025-04-06 21:06:38 -0400

    initial models for post and project implemented, blog landing page added

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BlogPost, Project
 
 # Register your models here.
+admin.site.register(BlogPost)
+admin.site.register(Project)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
 
+from website.models import BlogPost
+
+
 def home(request):
     """Sends the user to the home page"""
     return render(request, 'home.html', {})
+
+def blog(request):
+    """Sends the user to the blogs page"""
+    posts = BlogPost.objects.all()
+    return render(request, 'blogs.html', {'posts': posts})
